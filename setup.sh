36d1609fcbd31e781747aa91d047458faf7e5619
william <william.fromtw@gmail.com>
2020-09-22 17:03:42 +0800

    vmail directory user email

--- setup.sh
+++ setup.sh
@@ -32,6 +32,7 @@ if [ -n "${SEARCH_BASE}" ]; then
  sed -i "s/SEARCH_BASE/${SEARCH_BASE}/g" /etc/postfix/ldap-granted.cf
  sed -i "s/SEARCH_BASE/${SEARCH_BASE}/g" /etc/postfix/saslauthd.conf 
  sed -i "s/SEARCH_BASE/${SEARCH_BASE}/g" /etc/dovecot/dovecot-ldap.conf.ext 
+ sed -i "s/SEARCH_BASE/${SEARCH_BASE}/g" /etc/dovecot/dovecot-ldap2.conf.ext 
 fi
 
 if [ -n "${HOST_IP}" ]; then
@@ -43,6 +44,7 @@ if [ -n "${HOST_IP}" ]; then
  sed -i "s/HOST_IP/${HOST_IP}/g" /etc/postfix/ldap-granted.cf
  sed -i "s/HOST_IP/${HOST_IP}/g" /etc/postfix/saslauthd.conf 
  sed -i "s/HOST_IP/${HOST_IP}/g" /etc/dovecot/dovecot-ldap.conf.ext 
+ sed -i "s/HOST_IP/${HOST_IP}/g" /etc/dovecot/dovecot-ldap2.conf.ext 
  sed -i "s/HOST_IP/${HOST_IP}/g" /etc/crontab
  
 fi
@@ -56,6 +58,7 @@ if [ -n "${BIND_DN}" ]; then
  sed -i "s/BIND_DN/${BIND_DN}/g" /etc/postfix/ldap-granted.cf
  sed -i "s/BIND_DN/${BIND_DN}/g" /etc/postfix/saslauthd.conf
  sed -i "s/BIND_DN/${BIND_DN}/g" /etc/dovecot/dovecot-ldap.conf.ext 
+ sed -i "s/BIND_DN/${BIND_DN}/g" /etc/dovecot/dovecot-ldap2.conf.ext 
 fi
 
 if [ -n "${BIND_PW}" ]; then
@@ -67,6 +70,7 @@ if [ -n "${BIND_PW}" ]; then
  sed -i "s/BIND_PW/${BIND_PW}/g" /etc/postfix/ldap-granted.cf
  sed -i "s/BIND_PW/${BIND_PW}/g" /etc/postfix/saslauthd.conf
  sed -i "s/BIND_PW/${BIND_PW}/g" /etc/dovecot/dovecot-ldap.conf.ext 
+ sed -i "s/BIND_PW/${BIND_PW}/g" /etc/dovecot/dovecot-ldap2.conf.ext 
 fi
 
 if [ -n "${ALIASES}" ]; then
